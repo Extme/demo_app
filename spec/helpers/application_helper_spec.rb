c494d14f8bbacf9ec9b50eeca6e5cc30f4992984
Exitme <v_a_a@live.ru>
2016-02-05 18:55:12 +0200

    Finish layout and routes

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,16 @@
+ require 'spec_helper'
+
+ describe ApplicationHelper do
+ 
+
+    describe "full_title" do
+      it "shpuld include the page title" do
+        expect(full_title("foo")).to match(/foo/)
+      end
+
+      it "should include the base title" do
+        expect(full_title("foo")).to match(/^Ruby on Rails Tutorial Sample App/)
+      end
+
+    end
+ end
